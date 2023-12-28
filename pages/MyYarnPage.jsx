0af938b05f0a9bfa31bf1007af8c5f9d60909b89
TypeError92 <simonwalgenbach@posteo.de>
2023-12-28 20:47:44 +0000

    add skeleton list to my-yarn page

--- pages/MyYarnPage.jsx
+++ pages/MyYarnPage.jsx
@@ -1,7 +1,16 @@
 export function MyYarnPage(){
+    const yarnEntries = [{caption: 'Card 1', content: 'Content 1'}, {caption: 'Card 2', content: 'Content 2'}, {caption: 'Card 3', content: 'Content 3'}]
     return (
         <>
             <h1>MY YARN</h1>
+            <ul>
+                {yarnEntries.map(({caption, content}, i) => {
+                    return (<li key={i}>
+                        <h2>{caption}</h2>
+                        <p>{content}</p>
+                    </li>)
+                })}
+            </ul>
         </>
     )
 }
