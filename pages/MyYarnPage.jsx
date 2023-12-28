583d15e08e217e187f825135fdc1b27e2821c4ad
TypeError92 <simonwalgenbach@posteo.de>
2023-12-28 20:56:06 +0000

    add more dummy entries to test layout

--- pages/MyYarnPage.jsx
+++ pages/MyYarnPage.jsx
@@ -1,10 +1,14 @@
 export function MyYarnPage(){
-    const yarnEntries = [{caption: 'Card 1', content: 'Content 1'}, {caption: 'Card 2', content: 'Content 2'}, {caption: 'Card 3', content: 'Content 3'}]
+    const yarnEntries = []
+    for (let i=1; i<=25; i++){
+        yarnEntries.push({caption: `Caption ${i}`, content: `Content ${i}`})
+    }
     return (
         <>
             <h1>MY YARN</h1>
             <ul className="my-yarn__list">
                 {yarnEntries.map(({caption, content}, i) => {
+                    console.log(i)
                     return (<li key={i}>
                         <h2>{caption}</h2>
                         <p>{content}</p>
